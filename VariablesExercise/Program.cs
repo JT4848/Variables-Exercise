9c227cf8773b170f044fc2205bd16c4a3fcc6a8d
John Thomas Pricone <jtpricone@gmail.com>
2022-11-21 11:24:42 -0500

    changes

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "JT";
+            int myAge = 20;
+            bool loveFishing = true;
+            char exclamationPoint = '!';
+            decimal pi = 3.14159255359m;
+            double randomNumber = 48.48;
+
+            
+            
+            
+            
+            
+            
+            Console.WriteLine($"Hello, my name is {myName}, I am {myAge} years old");
+            Console.WriteLine($"If you ask if I like to fish, I would say {loveFishing} {exclamationPoint}");
+            Console.WriteLine($"My two favorite numbers are {pi} and {randomNumber} {exclamationPoint}");
+            
+            
+        
         }
     }
 }
